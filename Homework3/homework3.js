9eaccd9b0fcc88c155a3e37ae876d3045204fe1c
Kristijan Tutarkov <119169726+Tutarkov@users.noreply.github.com>
2023-01-30 17:16:02 +0100

    Add files via upload

---
+++ Homework3/homework3.js
@@ -0,0 +1,57 @@
+async function student() {
+
+    //const students = 'https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json';
+  
+    let response = await fetch("https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json"); 
+    let parsed = await response.json(); 
+    let sortirana = parsed;
+
+    let display = document.getElementById("result");
+    let male = document.getElementById("over");
+    let female = document.getElementById("grade");
+    let sorte = document.getElementById("sortiranje");
+
+    
+    const femaleStudents5 = parsed.filter(student => student.gender === 'Female' && student.averageGrade === 5);
+    const femaleStudentsList = document.querySelector("#femaleStudents5 ul");
+    femaleStudents5.forEach(student => {
+      const li = document.createElement("li");
+      li.textContent = `${student.firstName} ${student.lastName}`;
+      femaleStudentsList.appendChild(li);
+    });
+
+    const maleStudents18Skopje = parsed
+      .filter(student => student.gender === 'Male' && student.age >= 18 && student.city === 'Skopje')
+      .map(student => `${student.firstName} ${student.lastName}`);
+    const maleStudentsList = document.querySelector("#maleStudents18Skopje ul");
+    maleStudents18Skopje.forEach(fullName => {
+      const li = document.createElement("li");
+      li.textContent = fullName;
+      maleStudentsList.appendChild(li);
+    });
+
+    //Ova nesto ne raboti, zatoa pod nego so for e resena
+    // const femaleStudentsOver24 = parsed.filter(student => student.gender === 'Female' && student.age > 24);
+    // const avGrade = femaleStudentsOver24.reduce((acc, student) => acc + student.grade, 0) / femaleStudentsOver24.length;
+    // document.getElementById("femSt").innerHTML = `Female Students and averige grade, age>24 ${avGrade.toFixed(2)}`;
+
+    for (let i = 0; i < parsed.length; i++) {
+        if (parsed[i].age >= 24 && parsed[i].gender == 'Female') {
+                    female.innerHTML += `${parsed[i].firstName} ${parsed[i].averageGrade} <br>`;
+                }
+            }
+
+
+
+      const sortBy = property => (a, b) => a[property].localeCompare(b[property]);
+      const sortedSt = parsed.sort(sortBy("lastName"));
+      for (let i = 0; i < sortedSt.length; i++) {
+     sorte.innerHTML += `${sortedSt[i].lastName}  <br/>`;
+    
+    }
+
+} 
+student();
+
+
+
